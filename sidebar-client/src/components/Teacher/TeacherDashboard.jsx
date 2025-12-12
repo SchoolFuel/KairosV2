d88646b37229b5b666b5d28c5a8e34cf1bf3e785
Akhil Routhu <124550557+Routhu-Akhil@users.noreply.github.com>
2025-12-12 13:39:24 -0500

    Added Ignite Help and need help feature (#103)

--- sidebar-client/src/components/Teacher/TeacherDashboard.jsx
+++ sidebar-client/src/components/Teacher/TeacherDashboard.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ProjectQueueCard from "./ProjectQueue/ProjectQueue";
+import Ignite from "../Shared/Ignite/Ignite";
 
 export default function TeacherDashboard() {
   return (
     <>
       <ProjectQueueCard />
+      <Ignite />
+
       {/* Add more components here as needed */}
     </>
   );
